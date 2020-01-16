ae1fa3805838dc68b9c2895166eae22d15e8ea38
Graham Pennington <GrahamPennington@me.com>
2020-01-15 17:13:21 -0700

    Added GameManager to unity, added function Cookies to Var1 Script

--- FirstTestProject/Assets/Scripts/Assignments/Variables/Var1.cs
+++ FirstTestProject/Assets/Scripts/Assignments/Variables/Var1.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Var1 : MonoBehaviour
 {
+    public float amount;
+    public float money;
+    public float cost;
+        
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cookies(amount, money, cost);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Cookies(float amount, float money, float cost)
+    {
+        float total;
+        total = amount * cost - money;
+        print(total);
     }
 }
