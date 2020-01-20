51cbc80eece1953f9bcfc35b1b2bc00d68d6f551
Graham Pennington <GrahamPennington@me.com>
2020-01-20 13:08:11 -0700

    Added name variable and SetName Method

--- FirstTestProject/Assets/Scripts/Assignments/Variables/Var1.cs
+++ FirstTestProject/Assets/Scripts/Assignments/Variables/Var1.cs
@@ -7,6 +7,7 @@ public class Var1 : MonoBehaviour
     public float amount;
     public float money;
     public float cost;
+    public string name;
         
 
     // Start is called before the first frame update
@@ -18,7 +19,10 @@ public class Var1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("n"))
+        {
+            SetName();
+        }
     }
 
     void Cookies(float amount, float money, float cost)
@@ -26,5 +30,10 @@ public class Var1 : MonoBehaviour
         float total;
         total = money - (cost * amount);
         print(total);
+    }
+    void SetName()
+    {
+        name = "Bob";
+        Debug.Log("Huh, my name is " + name + " too!");
     }
 }
