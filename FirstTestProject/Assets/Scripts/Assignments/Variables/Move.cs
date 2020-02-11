ac9d81e0747db89cfe65ad98ac6e98014f7df037
Graham Pennington <GrahamPennington@me.com>
2020-02-10 23:40:16 -0700

    Changed to Character Controller

--- FirstTestProject/Assets/Scripts/Assignments/Variables/Move.cs
+++ FirstTestProject/Assets/Scripts/Assignments/Variables/Move.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed;
+    public float speed = 12f;
     public float verticalInput;
     public float horizontalInput;
-    public float rotationSpeed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    
+    Vector3 velocity;
+    bool isGrounded;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public CharacterController controller;
 
     public GameObject projectilePreFab;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {   
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+
+            velocity.y = -2f;
+        }
+
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        Vector3 move = (transform.right * horizontalInput + transform.forward * verticalInput).normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
         if(Input.GetMouseButtonDown(0)){
             Instantiate(projectilePreFab, transform.position, projectilePreFab.transform.rotation);
         }
-    }
-
-    void FixedUpdate(){
-        
-        transform.Translate(0,0,(verticalInput * speed * Time.deltaTime));
-        transform.Rotate(0,(horizontalInput * rotationSpeed * Time.deltaTime),0);
     }
 
 }
