9e32226493e6fb60dcb19fdc20ceeab9de020ff1
Graham Pennington <GrahamPennington@me.com>
2020-03-03 19:23:30 -0700

    Added Flashlight script to flashlight object to add togglibilty to light

---
+++ FirstTestProject/Assets/Flashlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    private Light light;
+
+    void Start(){
+        light = GetComponent<Light>();
+        light.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (light.enabled == true)
+            {  
+                light.enabled = false;
+                
+            }
+            else if (light.enabled == false)
+            {  
+                light.enabled = true;
+            
+            }
+        }
+
+        
+    }
+
+
+}
