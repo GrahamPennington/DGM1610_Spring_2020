bb8fb25460cdf70127bfdf456663bf323bf1d0c6
Graham Pennington <GrahamPennington@me.com>
2020-03-04 13:16:31 -0700

    Added flashlight model, added spot light with trigger that follows the player

---
+++ FirstTestProject/Assets/Scripts/Assignments/Variables/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Light light;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        light.enabled = false;
+
+        player = GameObject.Find("Player");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+    }
+
+    void OnTriggerEnter(Collider other) {
+
+       light.enabled = true;
+        
+    }
+}
